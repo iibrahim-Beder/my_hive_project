6f47a1272558f3df45beb3e083eecab24ff5cce0
Ibrahim Beder <ibeder518@gmail.com>
2025-03-20 23:23:33 +0200

    Practice on box & openBox

--- lib/test_screen.dart
+++ lib/test_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 class TestScreen extends StatelessWidget {
   const TestScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+
+            ElevatedButton(
+                onPressed: () async {
+                  await Hive.openBox<String>("test");
+                },
+                
+                child: Text("Open box")),
+
+                SizedBox(height: 30,),
+            ElevatedButton(
+                onPressed: () {
+                Box testBox =  Hive.box<String>("test");
+                },
+                child: Text("Access box"))
+          ],
+        ),
+      ),
+    );
   }
 }
